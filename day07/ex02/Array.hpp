7f5e68d348c55f66378714d6bebca7c263082f90
ghazali <aymenghazali1999@gmail.com>
2021-10-07 16:56:28 +0100

    d07 done

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -6,13 +6,82 @@
 /*   By: ayghazal <ayghazal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 11:29:14 by ayghazal          #+#    #+#             */
-/*   Updated: 2021/10/07 11:29:46 by ayghazal         ###   ########.fr       */
+/*   Updated: 2021/10/07 16:47:19 by ayghazal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
+#include <string>
 
+template<typename T>
+class Array {
+    private :
+    
+        T                *_array;
+        unsigned int        _len;
+    
+    public :
+        
+        Array()
+        {
+            _array = NULL;
+            _len = 0;   
+        };
+        
+        Array(unsigned int n)
+        {
+          _array = new T[n];
+          _len = n;  
+        };
+        
+        Array(Array const & src)
+		{
+			_array = new T[src._len];
+			for (unsigned int i = 0; i < src._len; i++)
+				_array[i] = src._array[i];
+			_len = src._len;
+		};
+        
+        virtual ~Array()
+		{
+			delete [] _array;
+		};
+        
+        Array	&	operator=(Array const & rhs)
+		{
+			if (_array)
+				delete [] _array;
+			_array = new T[rhs._len];
+			for (unsigned int i = 0; i < rhs._len; i++)
+				_array[i] = rhs._array[i];
+			_len = rhs._len;
+			return *this;
+        };
+        
+        T   &   operator[](int index)
+		{
+			if (index >= static_cast<int>(_len) || index < 0) 
+			{
+				throw OutOfLimits();
+			}
+			return _array[index];
+		};
+        
+        int size(void)
+		{
+			return (_len);
+		};
+        
+        class OutOfLimits : public std::exception
+		{
+			public:
+				OutOfLimits(void){return ;};
+				virtual ~OutOfLimits(void) throw(){return ;};
+				virtual const char    *what() const throw(){return("Index out of Limit !!");};
+		};
+};
 
 #endif
--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ayghazal <ayghazal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 11:29:12 by ayghazal          #+#    #+#             */
-/*   Updated: 2021/10/07 13:34:26 by ayghazal         ###   ########.fr       */
+/*   Updated: 2021/10/07 16:55:30 by ayghazal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 int     main()
 {
+    Array<int>				A;
+	Array<int>				B(4);
+	Array<std::string>		C(4);
+
+	for (int i = 0; i < B.size(); i++)
+		B[i] = i;
+	for (int i = 0; i < B.size(); i++)
+		std::cout << B[i] << " ";
+	std::cout << std::endl;
+	B[1] = 1337;
+	for (int i = 0; i < B.size(); i++)
+		std::cout << B[i] << " ";
+	std::cout << std::endl;
+	C[0] = "Hello";
+	C[1] = "BobiiiZ";
+	C[2] = "BobiiiiiiiiZ";
+	C[3] = "BooBiiiiiiiiiiiiiiiiZ";
+	for (int i = 0; i < C.size(); i++)
+		std::cout << C[i] << " ";
+	std::cout << std::endl;
+
     return 0;
 }
